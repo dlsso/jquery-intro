24d21ff0dc9485071cec6d42bc985af5e1848ac1
dlsso <supertonesfan@hotmail.com>
2014-06-25 11:12:35 -0600

    Added some jQuery

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
-$(document).on('ready', function() {
-  
-});
+// Select all elements that match the given css selector (passed as a string to the $ function)
+var welcome = $("#welcome-message")
+
+// First arguement it the CSS property you want to change
+// Second arguement is the value to change it to
+welcome.css("color", "blue")
+
+//Change multiple CSS properties at once (better to just make a CSS class and change to it)
+welcome.css({
+	"font-size": "72px",
+	"color": "red",
+	"font-weight": "bold"
+})
